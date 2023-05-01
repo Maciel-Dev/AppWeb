12393dab4a5b4f6b367f93a3ffc1ddc975418ed9
vinicius-je <vinicius.je99@gmail.com>
2023-04-30 22:57:26 -0300

    Event domain, request and response

---
+++ src/main/java/com/main/App/Payload/Request/EventRequest.java
@@ -0,0 +1,22 @@
+package com.main.App.Payload.Request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EventRequest {
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String description;
+    @NotBlank
+    private LocalDateTime dateTime;
+    @NotBlank
+    private String type;
+}
---
+++ src/main/java/com/main/App/Payload/Response/EventResponse.java
@@ -0,0 +1,19 @@
+package com.main.App.Payload.Response;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EventResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime dateTime;
+    private String type;
+}
