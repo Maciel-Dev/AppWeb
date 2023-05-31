70e8297b2b00423ffe03247f8c8f72f18939fa50
Maciel-Dev <jvmacielv@gmail.com>
2023-05-31 16:57:18 -0300

    Carregamento de Cartoes + Props + Autenticação

---
+++ front-end/src/components/CardPopup/CardPopup.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="fixed inset-0 flex items-center justify-center">
+    <div class="bg-white p-4 shadow-md rounded-lg">
+      <div class="text-lg font-bold">
+        <slot name="title"></slot>
+      </div>
+      <div>
+        <slot name="body"></slot>
+      </div>
+      <div class="mt-4">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardPopup',
+}
+</script>
